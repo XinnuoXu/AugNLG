c88b19056013a20dae7c5755830ac192ca89f950
XinnuoXu <xuxinnuo@yahoo.com>
2020-12-13 23:03:04 +0000

    comment out export gpu in .sh

--- decode.sh
+++ decode.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=2
+#export CUDA_VISIBLE_DEVICES=1
 
 #DOMAIN=taxi
 #DOMAIN=train
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DOMAIN=$1
-
-sh pre_train.sh $DOMAIN
-sh train.sh $DOMAIN
-sh decode.sh $DOMAIN
-sh evaluate.sh $DOMAIN
